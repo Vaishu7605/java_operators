895d9be4f3a5f83f89f102baa0268ded456c12e9
Vaishnavi Kesherwani <122589971+Vaishu7605@users.noreply.github.com>
2024-05-17 21:55:13 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,33 @@
+
+public class operators {
+
+    public static void main(String[] args) {
+
+        // Airthmetic operators
+        // 1.Addition
+        int sum1 = 100 + 50;
+        System.out.println(sum1);
+        int sum2 = sum1 + 250;
+        System.out.println(sum2);
+        int sum3 = sum1 + sum2;
+        System.out.println(sum3);
+
+        // 2. Substraction
+        int sub1 = 700 - 50;
+        System.out.println(sub1);
+        int sub2 = sub1 - 250;
+        System.out.println(sub2);
+        int sub3 = sub1 - sub2;
+        System.out.println(sub3);
+
+        // 3.Multiplication
+        // 4.Division
+        // 5. Modulus
+        // 6.Increment
+        // 7. Decrement
+        // Assignment Operators
+        // Comparision Operators
+        // Logical Operators
+        // Bitwise Operators
+    }
+}
